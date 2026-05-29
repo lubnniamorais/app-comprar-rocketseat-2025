8e26d44a5561a151bf8506e1887195ac4bf6cb49
lubnniamorais <lubnnia@gmail.com>
2026-05-29 10:27:47 -0300

    feat: Create storage folder

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,12 +1,22 @@
-import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import {
+  Alert,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
 import { Filter } from '@/components/Filter';
 import { Item } from '@/components/Item';
 
-import { styles } from './styles';
 import { FilterStatus } from '@/types/FilterStatus';
+import { itemsStorage, ItemStorage } from '@/storage/itemsStorage';
+
+import { styles } from './styles';
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 const ITENS = [
@@ -28,19 +38,63 @@ const ITENS = [
 ];
 
 export function Home() {
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+  const [description, setDescription] = useState('');
+  const [items, setItems] = useState<ItemStorage[]>([]);
+
+  const FILTER_STATUS: FilterStatus[] = [
+    FilterStatus.PENDING,
+    FilterStatus.DONE,
+  ];
+
+  function handleAdd() {
+    if (!description.trim()) {
+      return Alert.alert('Adicionar', 'Informe a descrição para adicionar');
+    }
+
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING,
+    };
+  }
+
+  async function getItems() {
+    try {
+      const response = await itemsStorage.get();
+      setItems(response);
+    } catch (error) {
+      console.log(error);
+
+      Alert.alert('Erro', 'Não foi possível filtrar os itens');
+    }
+  }
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo} />
 
       <View style={styles.form}>
-        <Input placeholder='O que você precisa comprar?' />
-        <Button title='Entrar' />
+        <Input
+          placeholder='O que você precisa comprar?'
+          onChangeText={setDescription}
+        />
+        <Button title='Adicionar' onPress={handleAdd} />
       </View>
 
       <View style={styles.content}>
         <View style={styles.header}>
           {FILTER_STATUS.map((status) => (
-            <Filter key={status} status={status} isActive />
+            <Filter
+              key={status}
+              status={status}
+              isActive={status === filter}
+              onPress={() => setFilter(status)}
+            />
           ))}
 
           <TouchableOpacity style={styles.clearButton}>
@@ -49,7 +103,7 @@ export function Home() {
         </View>
 
         <FlatList
-          data={ITENS}
+          data={items}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
             <Item
